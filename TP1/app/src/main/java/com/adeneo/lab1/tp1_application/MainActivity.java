418ad5c52c55476f324626b09d403d0bd28994a8
Unknown <natjolivet@hotmail.fr>
2018-09-16 16:53:39 +0200

    interface_fini

--- TP1/app/src/main/java/com/adeneo/lab1/tp1_application/MainActivity.java
+++ TP1/app/src/main/java/com/adeneo/lab1/tp1_application/MainActivity.java
@@ -2,12 +2,18 @@ package com.adeneo.lab1.tp1_application;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView commentaires;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
 }
